ee646ab5afe82e680f6b8d9f744366469f71fb8d
Darwin Guzmán <dga_guz@hotmail.es>
2020-08-11 01:13:52 -0500

    Configuraciones de CORS, pipes y variables de entorno

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+
+dotenv.config({ path: join(__dirname, '..', 'env', `${process.env.NODE_ENV}.env`) });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    },
+  ));
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(`Server is listening on ${process.env.NODE_ENV} environment with port ${port}`, 'Main');
 }
+
 bootstrap();
