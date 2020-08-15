c010b57e06c4934354b67e8714e2f8ceea45d44d
Darwin Guzmán <dga_guz@hotmail.es>
2020-08-14 20:48:29 -0500

    Servicio de inserción automático de tecnologías por defecto

--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -1,7 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { TecnologiaService } from './tecnologia/tecnologia.service';
 
 @Injectable()
-export class AppService {
+export class AppService implements OnModuleInit {
+  TECNOLOGIAS_DEFAULT = [
+    {
+      nombre: 'JavaScript',
+    },
+    {
+      nombre: 'TypeScript',
+    },
+    {
+      nombre: 'Java',
+    },
+    {
+      nombre: 'Kotlin',
+    },
+    {
+      nombre: 'Node.js',
+    },
+    {
+      nombre: 'Spring',
+    },
+    {
+      nombre: 'NestJS',
+    },
+    {
+      nombre: 'Express',
+    },
+    {
+      nombre: 'Angular',
+    },
+    {
+      nombre: 'Vue.js',
+    },
+    {
+      nombre: 'React.js',
+    },
+    {
+      nombre: 'PostgreSQL',
+    },
+    {
+      nombre: 'OracleDB',
+    },
+    {
+      nombre: 'SQLite',
+    },
+    {
+      nombre: 'SQL Server',
+    },
+    {
+      nombre: 'MongoDB',
+    },
+  ];
+
+  constructor(private readonly tecnologiaService: TecnologiaService) {
+  }
+
+  /**
+   * Se ejecuta una vez el módulo principal se haya desplegado y verifica si hay
+   * registros de tecnologías en la base de datos, si no hay ninguno entonces inserta
+   * tecnologías por defecto definidas en TECNOLOGIAS_DEFAULT.
+   */
+  async onModuleInit(): Promise<void> {
+    const tecnologiasEntities = await this.tecnologiaService.seleccionarTodos();
+    if (tecnologiasEntities.length === 0) {
+      for (const tecnologiaEntity of this.TECNOLOGIAS_DEFAULT) {
+        await this.tecnologiaService.insertar(tecnologiaEntity);
+      }
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
